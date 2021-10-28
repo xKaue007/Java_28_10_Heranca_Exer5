9d56300c7c82ba895f8cfa39faa0f44fa1f64e16
xKaue007 <kaueoliveira92@gmail.com>
2021-10-28 18:54:06 +0000

    3.0

--- Principal.java
+++ Principal.java
@@ -8,6 +8,15 @@ public class Principal{
     novo.setEndereco("Rua José Pereira Mendes");
     novo.setPreco(250000);
     novo.setAdicionalPreco(0.2*novo.getPreco());
-    System.out.println(novo.getPreco()+novo.novo.getAdicionalPreco());
+    System.out.println(novo.getPreco()+novo.getAdicionalPreco());
+
+    System.out.println("Imovel usado:");
+
+    velho.setEndereco("Vitor Eusébio da Silva");
+    velho.setPreco(230000);
+
+    velho.setDescontoPreco(0.1*velho.getPreco());
+    System.out.println("Preço do imóvel velho: "+(velho.getPreco()-velho.getDescontoPreco()));
+
     }
 }
